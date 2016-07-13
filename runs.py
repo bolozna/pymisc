b73aca84dee1649d665844a57bf2d3be3c76dace
Mikko Kivelä <6763161+bolozna@users.noreply.github.com>
2016-07-13 11:32:41 +0300

    Added parse_number to the runs model.

--- runs.py
+++ runs.py
@@ -13,3 +13,20 @@ def write_results(results,resultfilename):
     fcntl.flock(resultfile, fcntl.LOCK_UN)
     resultfile.close()
 
+
+def parse_number(inputstr):
+    """Parse numbers given as a string by the user.
+    """
+    try:
+        ilist=[float(inputstr)]
+    except ValueError:
+        if inputstr.startswith("linspace(") and inputstr[-1]==")":
+            inputstr=inputstr.strip("linspace(").strip(")")
+            vals=map(float,inputstr.split(","))
+            ilist=numpy.linspace(vals[0],vals[1],int(vals[2]))
+        elif inputstr.startswith("[") and inputstr[-1]=="]":
+            inputstr=inputstr.strip("[").strip("]")
+            ilist=map(float,inputstr.split(","))
+        else:
+            raise Exception("Invalid parameter: "+inputstr)
+    return ilist
